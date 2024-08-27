144dfac9fc9330ca41a706cba02d5c87fc8e5a28
MirossOffYT <74865590+MirossOFFYT@users.noreply.github.com>
2024-08-27 20:51:22 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,11 +48,11 @@ async def on_ready():
     while True:
         await asyncio.sleep(10)
         for guild in bot.guilds:
-            voice_channel = discord.utils.get(guild.voice_channels, name="General")
+            voice_channel = discord.utils.get(guild.voice_channels, name="Music")
 
             if not voice_channel:
                 # Si un canal vocal "General" n'existe pas, le créer
-                voice_channel = await guild.create_voice_channel("General")
+                voice_channel = await guild.create_voice_channel("Music")
                 vc = await voice_channel.connect()
                 await play_uvb_stream(vc)
             else:
